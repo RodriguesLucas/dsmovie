a026bceb79f0ad00f3537abfd37b2aed42ec39e5
Lucas Rodrigues <lucasrodrigued84@gmail.com>
2022-01-14 19:51:04 -0300

    Find Movies

---
+++ backend/src/main/java/com/devsuperior/dsmovie/services/MovieService.java
@@ -0,0 +1,30 @@
+package com.devsuperior.dsmovie.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import com.devsuperior.dsmovie.dto.MovieDTO;
+import com.devsuperior.dsmovie.entities.Movie;
+import com.devsuperior.dsmovie.repositories.MovieRepository;
+
+@Service
+public class MovieService {
+
+	@Autowired
+	private MovieRepository movieRepository;
+
+	@Transactional(readOnly = true)
+	public Page<MovieDTO> findAll(Pageable pageable) {
+		Page<Movie> lista = movieRepository.findAll(pageable);
+		return lista.map(x -> new MovieDTO(x));
+	}
+	
+	@Transactional(readOnly = true)
+	public MovieDTO findById(Long id) {
+		Movie result = movieRepository.findById(id).get();
+		return new MovieDTO(result);
+	}
+}
